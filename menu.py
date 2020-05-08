ca4b9d40447e449d35009823681afbc44de48c22
Matthew Meyerink <meyerink@umich.edu>
2020-05-08 18:03:19 -0400

    Harry Potter Font

--- menu.py
+++ menu.py
@@ -3,20 +3,24 @@ from tkinter import messagebox
 
 root = Tk()
 root.title("Hogwarts Adventure")
-root.geometry("600x400")
+root.geometry("1200x800")
 
-def in_progress():
-    msg = messagebox.showinfo("Game", "Hogwarts Under Construction!")
+def start_game():
+    main_title.destroy()
+    game_button.destroy()
+    quit_button.destroy()
+    construction_message = Label(root, text="Working on the game!", font=("Cochin", 40, "bold"))
+    construction_message.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 #Main Menu Title
-main_title = Label(root, text="Welcome to a Hogwarts Adventure!", justify=CENTER, font=("Helvetica",20,"bold"))
+main_title = Label(root, text="Welcome to a Hogwarts Adventure!", font=("Cochin", 40, "bold"))
 main_title.place(rely=0.25, relx=0.5, anchor=CENTER)
 
 #Main Game Menu
-game_button = Button(root, text="Start Game", justify=CENTER, command=in_progress)
-game_button.place(rely=0.50, relx=0.5, anchor=CENTER)
+game_button = Button(root, text="Start Game", font=("Cochin", 20, "bold"), justify=CENTER, padx=10, pady=10, command=start_game)
+game_button.place(rely=0.45, relx=0.5, anchor=CENTER)
 
-quit_button = Button(root, text="Quit", justify=CENTER, command=root.destroy)
+quit_button = Button(root, text="Quit", font=("Cochin", 20, "bold"), justify=CENTER, padx=10, pady=10, command=root.destroy)
 quit_button.place(rely=0.60, relx=0.5, anchor=CENTER)
 
 root.mainloop()
