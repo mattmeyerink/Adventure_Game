25f3fc41b12897bccb976bd0d66da75955c40a6b
Matthew Meyerink <meyerink@umich.edu>
2020-07-11 10:31:24 -0400

    Cleaned up instructions page

--- window.py
+++ window.py
@@ -41,18 +41,39 @@ class Window:
 
         window = Tk()
         window.title("Instructions")
-        window.geometry("500x300")
+        window.geometry("700x550")
 
-        main_title = Label(window, text="Instructions", font=("Cochin", 20, "bold"),
+        main_title = Label(window, text="Instructions", font=("Cochin", 30, "bold"),
             anchor=CENTER)
-        main_title.place(rely=0.1, relx=0.38)
+        main_title.pack()
 
-        list = Label(window, text="View your current item inventory on the right" +
-            " and any important messages\non the bottom\n\nControls\nw = move forward\n" +
-            "s = move backward\na = move left\nd = right\ni = interact\ne = exit room\n" +
+        list = Label(window, text="Attempt to find and destroy all of the horcruxes " +
+            "before attempting to destory\nLord Voldemort freeing the wizarding " +
+            "world from his tyranny.\n"
+
+            " View your current item inventory on the right" +
+            " and any important messages\non the bottom\n",
+            font=("Cochin", 20))
+        list.pack()
+
+        list2 = Label(window, text="Index", font=("Cochin", 24, "bold"))
+        list2.pack()
+
+        list3 = Label(window, text="Square = Magical Item\n" +
+            "Black Circle = Lord Voldemort\n" +
+            "Red Circle = Harry Potter\n" +
+            "Other Circles = Horcruxes\n",
+            font=("Cochin", 20))
+        list3.pack()
+
+        list4 = Label(window, text="Controls", font=("Cochin", 24, "bold"))
+        list4.pack()
+
+        list5 = Label(window, text="w = move forward\n" +
+            "s = move backward\na = move left\nd = move right\ni = interact\ne = exit room\n" +
             "f = fight enemy",
-            font=("Cochin", 14))
-        list.place(rely=0.3)
+            font=("Cochin", 20))
+        list5.pack()
 
         window.mainloop()
 
@@ -69,6 +90,6 @@ class Window:
 
         #Initialize and place the instructions button
         self.insturctions_button = Button(self.root, text="Instructions",
-            font=("Cochin", 14, "bold"), justify=CENTER, padx=10, pady=10,
+            font=("Cochin", 20, "bold"), justify=CENTER, padx=42.5, pady=39,
             command=self.print_instructions)
-        self.insturctions_button.place(relx=0.895, rely=0.94, anchor=NW)
+        self.insturctions_button.place(relx=0.80, rely=0.85, anchor=NW)
